464606195955248e69d2539214e61e9b296f77bf
Fernando <ribeiro.fer50@gmail.com>
2021-11-19 02:56:41 -0300

    hobby

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('hobbie').innerHTML = "My hobby";
+document.getElementById('name').innerHTML = "Fernando Celestrino Ribeiro";
+document.getElementById('age').innerHTML = "29 years old";
+document.getElementById('state').innerHTML = "Vitória/ES";
+document.getElementById('picture').src = "images/profile-picture.jpg";
+
+document.getElementById('tittle').innerHTML = "Cycling";
+document.getElementById('description').innerHTML = "Since i was a kid, i always liked riding my bike, and now that i grow up i can take bigger trips with it, if you want to keep up with, follow me on strava";
+document.getElementById('links').href = "https://www.strava.com/athletes/7726378";
+document.getElementById('links').innerHTML = "STRAVA";
+document.getElementById('image1').src = "images/bike01.jpeg";
+document.getElementById('image3').src = "images/bike03.jpeg";
+
